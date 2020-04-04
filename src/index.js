f778f32649924499b25dd9590f37d008e3fb7c3f
Gio Justiniano <giojustiniano@Gios-Air.home>
2020-04-04 21:36:00 +0200

    Add react primary files

---
+++ src/index.js
@@ -0,0 +1,20 @@
+/*
+ * Created on Sat Apr 04 2020
+ * Author: Gio Justiniano
+ * More info: https://github.com/giomikee/
+ *
+ * Copyright (c) 2020
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.css';
+import App from './components/App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
