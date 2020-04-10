55b3042d358024f1ccc661571e5ed13460858524
Gio Justiniano <giojustiniano@Gios-Air.home>
2020-04-10 12:17:46 +0200

    Updates

---
+++ precache-manifest.260bcca2aef40d7fa4fabcef92a5e447.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f61b79c51ec460cc4f414754dd6c773",
+    "url": "/currency-converter/index.html"
+  },
+  {
+    "revision": "ce88f5c2494376652da1",
+    "url": "/currency-converter/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "65890188d8e24444a74b",
+    "url": "/currency-converter/static/css/main.7cfaebc9.chunk.css"
+  },
+  {
+    "revision": "ce88f5c2494376652da1",
+    "url": "/currency-converter/static/js/2.e58823f4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/currency-converter/static/js/2.e58823f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65890188d8e24444a74b",
+    "url": "/currency-converter/static/js/main.313442f8.chunk.js"
+  },
+  {
+    "revision": "e35598be14bcfb91617b",
+    "url": "/currency-converter/static/js/runtime-main.6abfa321.js"
+  }
+]);
